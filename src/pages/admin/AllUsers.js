9eeb5462fbc23ce4714e9b59e4e7e54157d49815
Milos <m.djuka@hotmail.com>
2023-08-30 16:43:08 +0200

    added users table

--- src/pages/admin/AllUsers.js
+++ src/pages/admin/AllUsers.js
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+import useHttp from "../../hooks/use-http";
+import TableHeader from "../../components/TableHeader";
 
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  const theme = useTheme();
+  const [data, setData] = useState([]);
+  const { isLoading, sendRequest } = useHttp();
+
+  useEffect(() => {
+    const getUsers = (usersData) => {
+      console.log(usersData);
+      setData(usersData);
+    };
+
+    sendRequest(
+      {
+        url: "/users",
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      getUsers
+    );
+  }, [sendRequest]);
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "roles",
+      headerName: "Roles",
+      flex: 1,
+    },
+    {
+      field: "firstName",
+      headerName: "First Name",
+      flex: 1,
+    },
+    {
+      field: "lastName",
+      headerName: "Last Name",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box m="2.5rem 2.5rem">
+      <TableHeader title="Users" subtitle={`${data.length} Users`} />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            color: theme.palette.grey[300],
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: `1px solid ${theme.palette.grey[800]} !important`,
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            borderBottom: `1px solid ${theme.palette.grey[800]} !important`,
+          },
+          "& .MuiDataGrid-columnSeparator": {
+            visibility: "hidden",
+          },
+          "& .MuiTablePagination-root": {
+            color: theme.palette.grey[300],
+          },
+          "& .MuiSelect-icon": {
+            color: theme.palette.grey[300],
+          },
+          "& .MuiIconButton-root": {
+            color: `${theme.palette.grey[300]} !important`,
+          },
+        }}
+      >
+        <DataGrid loading={isLoading || !data} getRowId={(row) => row._id} rows={data || []} columns={columns} />
+      </Box>
+    </Box>
+  );
 };
 
 export default AllUsers;
