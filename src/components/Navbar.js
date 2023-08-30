2652918a38af12b5a8cee99119d373cf0a19e6c6
Milos <m.djuka@hotmail.com>
2023-08-30 16:42:21 +0200

    added user profile, and handle logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,54 @@
-import React from "react";
-import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from "@mui/icons-material";
+import React, { useState } from "react";
+import { Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { AppBar, IconButton, InputBase, Toolbar, useTheme, Button, Menu, MenuItem, Box, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 import FlexBetween from "./FlexBetween";
-import { useDispatch } from "react-redux";
-import profileImage from "../assets/profile.jpg";
-import { AppBar, IconButton, InputBase, Toolbar, useTheme } from "@mui/material";
+import useHttp from "../hooks/use-http";
+import { authActions } from "../store/auth";
 
 const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const theme = useTheme();
+  const { sendRequest } = useHttp();
+  const userFirstName = useSelector((state) => state.firstName);
+  const userLastName = useSelector((state) => state.lastName);
+  const userProfileImage = useSelector((state) => state.profileImage);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+
+  const handleLogout = () => {
+    setAnchorEl(null);
+
+    const logoutUser = () => {
+      dispatch(authActions.logout());
+      localStorage.removeItem("user");
+      navigate("/login");
+    };
+
+    sendRequest(
+      {
+        url: "/logout",
+        method: "GET",
+      },
+      logoutUser
+    );
+  };
+
+  const serverAddress = process.env.ENVIRONMENT === "production" ? process.env.REACT_APP_PROD_BASE_URL : process.env.REACT_APP_DEV_BASE_URL;
+
   return (
     <AppBar
       sx={{
         position: "static",
         background: "none",
         boxShadow: "none",
-      }}>
+      }}
+    >
       <Toolbar sx={{ justifyContent: "space-between" }}>
         <FlexBetween>
           <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
@@ -40,7 +75,6 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
           </FlexBetween>
         </FlexBetween>
         <FlexBetween gap="0.5rem">
-          {/* TODO: 1:00:22 crvena, 45:02 zelena  */}
           <IconButton>
             <SettingsOutlined
               sx={{
@@ -48,6 +82,52 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
               }}
             />
           </IconButton>
+
+          {/* PROFILE */}
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform: "none",
+                gap: "1rem",
+              }}
+            >
+              <Box component="img" alt="profile" src={serverAddress + "/" + userProfileImage} crossOrigin="use-credentials" height="32px" width="32px" borderRadius="50%" sx={{ objectFit: "cover" }} />
+              <Box textAlign="left">
+                <Typography fontWeight="bold" variant="h4">
+                  {userFirstName}
+                </Typography>
+                <Typography fontWeight="bold" variant="h4">
+                  {userLastName}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined sx={{ color: theme.palette.secondary[300], fontSize: "25px" }} />
+            </Button>
+            <Menu
+              anchorEl={anchorEl}
+              open={isOpen}
+              onClose={() => setAnchorEl(null)}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              // sx={{
+              //   "& .MuiList-root": {
+              //     backgroundColor: "black",
+              //   },
+              // }}
+            >
+              <MenuItem
+                onClick={handleLogout}
+                // sx={{
+                //   backgroundColor: "black",
+                //   m: "0",
+                // }}
+              >
+                Log Out
+              </MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
