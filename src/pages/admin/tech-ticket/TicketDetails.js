90d161d1ba786566246412fbf11f5af7004dbca4
Milos <m.djuka@hotmail.com>
2023-09-14 15:27:39 +0200

    show ticket metadata, handle ticket status change, and ticket logging

--- src/pages/admin/tech-ticket/TicketDetails.js
+++ src/pages/admin/tech-ticket/TicketDetails.js
@@ -1,18 +1,27 @@
 import { Fragment, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Box, Typography, useTheme, useMediaQuery, Divider, Select, MenuItem, FormControl } from "@mui/material";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Box, Typography, useTheme, useMediaQuery, Divider, Select, MenuItem, FormControl, IconButton, Tooltip } from "@mui/material";
+import { SettingsOutlined, Delete } from "@mui/icons-material";
 
 import useHttp from "../../../hooks/use-http";
+import FlexBetween from "../../../components/FlexBetween";
+import AlertDialog from "../../../components/AlertDialog";
 
 const serverAddress = process.env.ENVIRONMENT === "production" ? process.env.REACT_APP_PROD_BASE_URL : process.env.REACT_APP_DEV_BASE_URL;
 
 const TicketDetails = () => {
   const theme = useTheme();
   const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const navigate = useNavigate();
+  const userId = useSelector((state) => state.userId);
   const { ticketId } = useParams();
   const { isLoading, sendRequest } = useHttp();
+  const { sendRequest: changeTicketStatus } = useHttp();
   const [data, setData] = useState(null);
+  const [ticketLogs, setTicketLogs] = useState(null);
   const [ticketStatus, setTicketStatus] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
 
   const getDate = (dateStr) => {
     const date = new Date(dateStr);
@@ -29,6 +38,7 @@ const TicketDetails = () => {
     const getTicketDetails = (ticketData) => {
       setData(ticketData);
       setTicketStatus(ticketData.status);
+      setTicketLogs(ticketData.logs);
       console.log(ticketData);
     };
 
@@ -44,12 +54,71 @@ const TicketDetails = () => {
     );
   }, [sendRequest, ticketId]);
 
+  const changeTicketStatusHandler = async (event) => {
+    const updateTicket = (ticketData) => {
+      setTicketStatus(ticketData.status);
+    };
+
+    changeTicketStatus(
+      {
+        url: "/techTickets",
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          id: ticketId,
+          status: event.target.value,
+        },
+      },
+      updateTicket
+    );
+
+    const updateTicketLogs = (logData) => {
+      setTicketLogs((prevVal) => {
+        return [...prevVal, logData];
+      });
+    };
+
+    changeTicketStatus(
+      {
+        url: `/techTickets/${ticketId}/ticketLog`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          message: `Ticket status was changed to ${event.target.value}`,
+          userId,
+        },
+      },
+      updateTicketLogs
+    );
+  };
+
+  const deleteTicketHandler = async () => {
+    const getTicketDetails = (ticketData) => {
+      navigate("/admin/tickets/");
+    };
+
+    sendRequest(
+      {
+        url: `/techTickets/${ticketId}`,
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      getTicketDetails
+    );
+  };
+
   return (
     <Fragment>
       {data && !isLoading && (
         <Box display="flex" flexWrap="wrap" margin="2rem" gap="2rem">
           {/* LEFT SIDE */}
-          <Box flexGrow="1" width={isNonMobile ? "32%" : "100%"} backgroundColor={theme.palette.background.light} p="1rem" borderRadius="5px">
+          <Box flexGrow="1" width={isNonMobile ? "32%" : "100%"} height="fit-content" backgroundColor={theme.palette.background.light} p="1rem" borderRadius="5px">
             <Box display="flex" gap="1rem" alignItems="center">
               <Box
                 component="img"
@@ -75,59 +144,123 @@ const TicketDetails = () => {
             </Box>
           </Box>
           {/* RIGHT SIDE */}
-          <Box flexGrow={isNonMobile ? "0.6" : "1"} width="10%" backgroundColor={theme.palette.background.light} p="1rem" borderRadius="5px">
-            <Typography variant="h3" mb="2rem">
-              Ticket Details
-            </Typography>
-            <Box display="flex" gap="1rem">
-              <Typography color={theme.palette.grey[500]}>Ticket ID:</Typography>
-              <Typography color={theme.palette.grey[200]}>{data._id}</Typography>
+          <Box display="flex" flexDirection="column" gap="2rem" flexGrow={isNonMobile ? "0.6" : "1"} width="10%" height="fit-content">
+            {/* TICKET DETAILS */}
+            <Box backgroundColor={theme.palette.background.light} p="1rem" borderRadius="5px">
+              <Typography variant="h3" mb="2rem">
+                Ticket Details
+              </Typography>
+              <Box display="flex" gap="1rem">
+                <Typography color={theme.palette.grey[500]}>Ticket ID:</Typography>
+                <Typography color={theme.palette.grey[200]}>{data._id}</Typography>
+              </Box>
+              <Divider sx={{ borderColor: theme.palette.grey[700], mt: "1rem", mb: "1rem" }} />
+              <Box display="flex" gap="1rem">
+                <Typography color={theme.palette.grey[500]}>User:</Typography>
+                <Typography color={theme.palette.grey[200]}>{`${data.user.firstName} ${data.user.lastName}`}</Typography>
+              </Box>
+              <Divider sx={{ borderColor: theme.palette.grey[700], mt: "1rem", mb: "1rem" }} />
+              <Box display="flex" gap="1rem">
+                <Typography color={theme.palette.grey[500]}>User Email:</Typography>
+                <Typography color={theme.palette.grey[200]}>{data.user.email}</Typography>
+              </Box>
+              <Divider sx={{ borderColor: theme.palette.grey[700], mt: "1rem", mb: "1rem" }} />
+              <Box display="flex" gap="1rem">
+                <Typography color={theme.palette.grey[500]}>Created At:</Typography>
+                <Typography color={theme.palette.grey[200]}>{getDate(data.createdAt)}</Typography>
+              </Box>
+              <Divider sx={{ borderColor: theme.palette.grey[700], mt: "1rem", mb: "1rem" }} />
+              <Box display="flex" gap="1rem">
+                <Typography color={theme.palette.grey[500]}>Updated At:</Typography>
+                <Typography color={theme.palette.grey[200]}>{getDate(data.updatedAt)}</Typography>
+              </Box>
+              <Divider sx={{ borderColor: theme.palette.grey[700], mt: "1rem", mb: "1rem" }} />
+              <Box display="flex" gap="1rem">
+                <Typography color={theme.palette.grey[500]}>Category:</Typography>
+                <Typography color={theme.palette.grey[200]}>{data.category}</Typography>
+              </Box>
+              <FlexBetween display="flex" gap="0.5rem" mt="3rem">
+                <Box display="flex" alignItems="center" gap="1rem">
+                  <Typography color={theme.palette.grey[500]}>Status:</Typography>
+                  <FormControl variant="standard">
+                    <Select
+                      value={ticketStatus}
+                      onChange={changeTicketStatusHandler}
+                      sx={{
+                        "::before": {
+                          borderBottom: `1px solid ${theme.palette.grey[200]}`,
+                        },
+                        color: theme.palette.grey[200],
+                        "& .MuiSvgIcon-root": {
+                          color: theme.palette.grey[200],
+                        },
+                      }}
+                    >
+                      <MenuItem value="New">New</MenuItem>
+                      <MenuItem value="In Progress">In Progress</MenuItem>
+                      <MenuItem value="Closed">Closed</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Box>
+                  <Tooltip title="Edit" placement="top" arrow>
+                    <IconButton
+                      onClick={() => {
+                        navigate(`/admin/tickets/edit/${ticketId}`);
+                      }}
+                    >
+                      <SettingsOutlined
+                        sx={{
+                          color: theme.palette.grey.main,
+                        }}
+                      />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="Delete" placement="top" arrow>
+                    <IconButton
+                      onClick={() => {
+                        setOpenDialog(true);
+                      }}
+                    >
+                      <Delete
+                        sx={{
+                          color: theme.palette.grey.main,
+                        }}
+                      />
+                    </IconButton>
+                  </Tooltip>
+                  <AlertDialog title="Delete Ticket?" content="Are you sure you want to delete this ticket?" open={openDialog} setOpen={setOpenDialog} handleConfirm={deleteTicketHandler} />
+                </Box>
+              </FlexBetween>
             </Box>
-            <Divider sx={{ borderColor: theme.palette.grey[200], mt: "1rem", mb: "1rem" }} />
-            <Box display="flex" gap="1rem">
-              <Typography color={theme.palette.grey[500]}>User:</Typography>
-              <Typography color={theme.palette.grey[200]}>{`${data.user.firstName} ${data.user.lastName}`}</Typography>
-            </Box>
-            <Divider sx={{ borderColor: theme.palette.grey[200], mt: "1rem", mb: "1rem" }} />
-            <Box display="flex" gap="1rem">
-              <Typography color={theme.palette.grey[500]}>User Email:</Typography>
-              <Typography color={theme.palette.grey[200]}>{data.user.email}</Typography>
-            </Box>
-            <Divider sx={{ borderColor: theme.palette.grey[200], mt: "1rem", mb: "1rem" }} />
-            <Box display="flex" gap="1rem">
-              <Typography color={theme.palette.grey[500]}>Created At:</Typography>
-              <Typography color={theme.palette.grey[200]}>{getDate(data.createdAt)}</Typography>
-            </Box>
-            <Divider sx={{ borderColor: theme.palette.grey[200], mt: "1rem", mb: "1rem" }} />
-            <Box display="flex" gap="1rem">
-              <Typography color={theme.palette.grey[500]}>Updated At:</Typography>
-              <Typography color={theme.palette.grey[200]}>{getDate(data.updatedAt)}</Typography>
-            </Box>
-            <Divider sx={{ borderColor: theme.palette.grey[200], mt: "1rem", mb: "1rem" }} />
-            <Box display="flex" gap="1rem">
-              <Typography color={theme.palette.grey[500]}>Category:</Typography>
-              <Typography color={theme.palette.grey[200]}>{data.category}</Typography>
-            </Box>
-            <Box display="flex" gap="1rem" mt="3rem">
-              <FormControl variant="standard">
-                <Select
-                  value={ticketStatus}
-                  onChange={(event) => setTicketStatus(event.target.value)}
-                  sx={{
-                    "::before": {
-                      borderBottom: `1px solid ${theme.palette.grey[200]}`,
-                    },
-                    color: theme.palette.grey[200],
-                    "& .MuiSvgIcon-root": {
-                      color: theme.palette.grey[200],
-                    },
-                  }}
-                >
-                  <MenuItem value="New">New</MenuItem>
-                  <MenuItem value="In Progress">In Progress</MenuItem>
-                  <MenuItem value="Closed">Closed</MenuItem>
-                </Select>
-              </FormControl>
+            {/* TICKET HISTORY */}
+            <Box backgroundColor={theme.palette.background.light} p="1rem" borderRadius="5px">
+              <Typography variant="h3" mb="2rem">
+                Ticket History
+              </Typography>
+              {ticketLogs.toReversed().map((l) => {
+                return (
+                  <Box key={l._id}>
+                    <Box display="flex" gap="1rem">
+                      <Box
+                        component="img"
+                        alt="profile"
+                        src={serverAddress + "/" + l.user.profileImage}
+                        crossOrigin="use-credentials"
+                        height="32px"
+                        width="32px"
+                        borderRadius="50%"
+                        sx={{ objectFit: "cover" }}
+                      />
+                      <Box>
+                        <Typography color={theme.palette.grey[200]}>{l.message}</Typography>
+                        <Typography color={theme.palette.grey[200]}>{getDate(l.createdAt)}</Typography>
+                      </Box>
+                    </Box>
+                    <Divider sx={{ borderColor: theme.palette.grey[700], mt: "1rem", mb: "1rem" }} />
+                  </Box>
+                );
+              })}
             </Box>
           </Box>
         </Box>
