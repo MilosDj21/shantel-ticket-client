0ab57e62e5220a5ae4215c1d1d50ae2f5bca0b04
Milos <m.djuka@hotmail.com>
2024-02-01 15:59:48 +0100

    handle table button click

--- src/pages/project/ProjectDetails.js
+++ src/pages/project/ProjectDetails.js
@@ -21,6 +21,10 @@ const ProjectDetails = () => {
   const userId = useSelector((state) => state.userId);
   const { projectId } = useParams();
   const { isLoading, sendRequest } = useHttp();
+  const { sendRequest: saveTitleSendRequest } = useHttp();
+  const { sendRequest: changeProgressSendRequest } = useHttp();
+  const { sendRequest: createGroupSendRequest } = useHttp();
+  const { sendRequest: createTaskSendRequest } = useHttp();
   const [data, setData] = useState("");
   const [search, setSearch] = useState("");
 
@@ -35,30 +39,6 @@ const ProjectDetails = () => {
         },
       },
       (projectData) => {
-        const columnNames = new Set();
-        const columns = [];
-        for (const post of projectData.postRequests) {
-          for (const task of post.tasks) {
-            columnNames.add(task.group.title);
-          }
-        }
-        for (const colName of columnNames.values()) {
-          const col = {
-            _id: "",
-            title: colName,
-            tasks: [],
-          };
-          for (const post of projectData.postRequests) {
-            for (const task of post.tasks) {
-              if (colName === task.group.title) {
-                col._id = task.group._id;
-                col.tasks.push({ title: post.title, ...task });
-              }
-            }
-          }
-          columns.push(col);
-        }
-        projectData.columnArray = columns;
         console.log(projectData);
         setData(projectData);
       }
@@ -90,10 +70,120 @@ const ProjectDetails = () => {
   };
 
   const buttonClickHandle = async (params) => {
+    let nextStep = "";
+    let groupTitle = "";
+    let groupId = null;
+    if (params.progressLevel === "doneCheck") {
+      nextStep = "pendingWrite";
+      groupTitle = "Article Writing";
+    } else if (params.progressLevel === "doneWrite") {
+      nextStep = "pendingPublish";
+      groupTitle = "Post Publishing";
+    }
+    for (const group of data.groups) {
+      if (group.title === groupTitle) {
+        groupId = group._id;
+      }
+    }
+
+    const createTask = (finalGroupId) => {
+      // create new task for next step
+      createTaskSendRequest(
+        {
+          url: `/postTasks`,
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: {
+            title: params.title,
+            dueTime: params.urgencyLevel,
+            post: params._id,
+            group: finalGroupId,
+          },
+        },
+        (taskData) => {
+          // TODO: privremeno resenje posto kad se kreira novi ne povlaci poruke
+          taskData.messages = [];
+          for (const group of data.groups) {
+            if (group._id === finalGroupId) {
+              group.tasks.push(taskData);
+            }
+          }
+          console.log("task:", taskData);
+        }
+      );
+    };
+
+    // change progress level
+    changeProgressSendRequest(
+      {
+        url: `/postRequests/${params._id}`,
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          progressLevel: nextStep,
+        },
+      },
+      (postData) => {
+        console.log("post", postData);
+        for (const post of data.postRequests) {
+          if (post._id === params._id) {
+            post.progressLevel = nextStep;
+          }
+        }
+        // if group is not already in project create new group
+        if (!groupId) {
+          // create new task group for next step
+          createGroupSendRequest(
+            {
+              url: `/postTaskGroups`,
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: {
+                title: groupTitle,
+                project: projectId,
+              },
+            },
+            (groupData) => {
+              // TODO: privremeno resenje posto kad se kreira novi ne povlaci taskove
+              groupData.tasks = [];
+              data.groups.push(groupData);
+              console.log("group:", groupData);
+              // create new task for next step
+              createTask(groupData._id);
+            }
+          );
+        } else {
+          createTask(groupId);
+        }
+      }
+    );
+
     console.log(params);
   };
 
   const saveTitleHandler = async (text) => {
+    // save updated project title
+    saveTitleSendRequest(
+      {
+        url: `/projects/${projectId}`,
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          title: text,
+        },
+      },
+      (projectData) => {
+        console.log(projectData);
+      }
+    );
     console.log(text);
   };
 
@@ -205,22 +295,47 @@ const ProjectDetails = () => {
       flex: 0.7,
       renderCell: (params) => {
         let buttonText = "";
+        let ready = false;
         switch (params.value) {
+          case "pendingCheck":
+            buttonText = "Waiting For Check";
+            break;
           case "doneCheck":
             buttonText = "Send to Write";
+            ready = true;
+            break;
+          case "pendingWrite":
+            buttonText = "Waiting For Writer";
             break;
           case "doneWrite":
             buttonText = "Send to Publish";
+            ready = true;
+            break;
+          case "pendingPublish":
+            buttonText = "Waiting For Publish";
+            break;
+          case "donePublish":
+            buttonText = "Published";
             break;
           default:
             buttonText = "";
         }
-        return buttonText.length > 0 ? (
+        return ready > 0 ? (
           <Button
             variant="contained"
             onClick={(e) => {
               e.stopPropagation();
-              buttonClickHandle(params.id);
+              buttonClickHandle(params.row);
+            }}
+          >
+            {buttonText}
+          </Button>
+        ) : buttonText.length > 0 ? (
+          <Button
+            disabled
+            sx={{
+              color: "#000 !important",
+              backgroundColor: theme.palette.grey[600],
             }}
           >
             {buttonText}
@@ -290,7 +405,7 @@ const ProjectDetails = () => {
       {/* TASKS DETAILS */}
       {data && !isLoading && (
         <Box display="flex" m="2.5rem 2.5rem">
-          {data.columnArray.map((c) => {
+          {data.groups.map((c) => {
             return <TaskColumn key={c._id} column={c} />;
           })}
         </Box>
