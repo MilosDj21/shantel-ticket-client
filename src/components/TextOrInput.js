799979382686e8331d5a198cab1d990b099035ea
Milos <m.djuka@hotmail.com>
2024-02-19 13:45:04 +0100

    handle change in multiple different db tables

--- src/components/TextOrInput.js
+++ src/components/TextOrInput.js
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import { InputBase, useTheme } from "@mui/material";
 
-const TextOrInput = ({ fontSize, textValue, callback }) => {
+const TextOrInput = ({ fontSize, textValue, callback, fieldToUpdate = null, tableToUpdate = null }) => {
   const theme = useTheme();
   const [text, setText] = useState(textValue);
+  const [tempVal, settempVal] = useState(textValue.toString().slice());
+
+  const onBlurHandler = async () => {
+    if (fieldToUpdate && tableToUpdate) {
+      const isLoading = await callback(text, fieldToUpdate, tableToUpdate);
+      if (isLoading) setText(tempVal.toString().slice(0));
+    } else {
+      callback(text);
+    }
+  };
 
   return (
     <InputBase
       type="text"
       sx={{
-        width: "100%",
+        flex: "1",
+        maxWidth: "100%",
         color: theme.palette.grey[300],
         fontSize: { fontSize },
         m: "0.4rem 0",
@@ -18,20 +29,18 @@ const TextOrInput = ({ fontSize, textValue, callback }) => {
         },
         "& .MuiInputBase-input:hover": {
           border: `1px solid ${theme.palette.grey[800]}`,
-          borderRadius: "9px",
+          borderRadius: "5px",
         },
         "& .MuiInputBase-input:focus": {
           border: `1px solid ${theme.palette.grey[800]}`,
-          borderRadius: "9px",
+          borderRadius: "5px",
         },
       }}
       value={text}
       onChange={(e) => {
         setText(e.target.value);
       }}
-      onBlur={() => {
-        callback(text);
-      }}
+      onBlur={onBlurHandler}
     />
   );
 };
